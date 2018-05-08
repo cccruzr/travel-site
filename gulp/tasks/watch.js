fdbbe26848092aeabbab17a81b11d669908bfdd3
Camilo Cruz <cccruzr@gmail.com>
2018-05-08 15:31:04 -0500

    Completed: Large Hero + Intro Content blocks.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('cssInject', ['styles'], function() { //'styles' is a dependency of cssInject
+    return gulp.src('./travel-app/temp/styles/styles.css')
+               .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "travel-app"
+    }
+  });
+
+  watch('./travel-app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./travel-app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });	
+});
